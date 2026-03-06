4af634fc2741bc425d53a755c7938a3ed666a887
bea <bgallorente@gmail.com>
2026-03-06 18:34:36 +0100

    organizacion

--- entrega3/productoSegundaMano.java
+++ entrega3/productoSegundaMano.java
@@ -1,4 +1,17 @@
-package entrega3;
+
+
 public class productoSegundaMano {
-    
+    private boolean estaDisponible;
+    private ClienteRegistrado propietario;
+    private int valoracion;
+    private double valorEstimado;
+    private boolean estaValorado;
+
+    public productoSegundaMano(ClienteRegistrado propietario, double valorEstimado) {
+        this.estaDisponible = true;
+        this.propietario = propietario;
+        this.valoracion = 0;
+        this.valorEstimado = valorEstimado;
+        this.estaValorado = false;
+    }
 }
