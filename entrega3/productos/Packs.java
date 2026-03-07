c8a9a3209a1bcdb5797343fe0c8ee0b3abc16c8c
ale <alejandra.zurdo@estudiante.uam.es>
2026-03-07 13:35:04 +0100

    packs base hecho

--- entrega3/productos/Packs.java
+++ entrega3/productos/Packs.java
@@ -1,5 +1,33 @@
 package productos;
 
-public class Packs {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pack {
+    private final String nombre;
+    private final List<Producto> productos;
     
+    private double precio;
+
+    public Pack(String nombre, double precio, List<Producto> productos) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.productos = new ArrayList<>(productos); 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public List<Producto> getProductos() {
+        return new ArrayList<>(productos);
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
