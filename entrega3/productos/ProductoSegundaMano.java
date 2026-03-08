59709dc44ea2c8cff3fb482a3ce200548679e3ae
bea <bgallorente@gmail.com>
2026-03-08 23:19:14 +0100

    productoSegundaMano

--- entrega3/productos/ProductoSegundaMano.java
+++ entrega3/productos/ProductoSegundaMano.java
@@ -2,7 +2,7 @@ package productos;
 import usuarios.ClienteRegistrado;
 
 
-public class ProductoSegundaMano {
+public class ProductoSegundaMano extends Producto {
     private boolean estaDisponible;
     private ClienteRegistrado propietario;
     private int valoracion;
@@ -16,6 +16,8 @@ public class ProductoSegundaMano {
         this.propietario = propietario;
         this.estaDisponible = false;
         this.estaValorado = false;
+        this.valoracion = 0;
+        this.valorEstimado = 0.0;
         this.estadoProducto = EstadoProducto.PENDIENTE_DE_VALORAR;
     }
 
@@ -30,10 +32,31 @@ public class ProductoSegundaMano {
         }
     }
 
+    public void borrarProducto() {
+        this.estaDisponible = false;
+    }
+
     public void setValoracion(int valoracion, double valorEstimado, EstadoConservacion estadoConservacion) {
         this.valoracion = valoracion;
         this.valorEstimado = valorEstimado;
         this.estadoConservacion = estadoConservacion;
         this.estaValorado = true;
+        this.estadoProducto = EstadoProducto.VALORADO;
     }
-}
+
+    public int getValoracion() {
+        return valoracion;
+    }
+
+    public boolean estaDisponible() {
+        return estaDisponible;
+    }
+
+    public EstadoProducto getEstadoProducto() {
+        return estadoProducto;
+    }
+
+    public ClienteRegistrado getPropietario() {
+        return propietario;
+    }
+} 
