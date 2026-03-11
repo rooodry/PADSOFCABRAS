d07cec56c9320815b725a8a67c1c371b184707fe
rooodry <moralcruzrodrigo@gmail.com>
2026-03-11 18:35:17 +0100

    d

--- entrega3/usuarios/EmpleadoIntercambio.java
+++ entrega3/usuarios/EmpleadoIntercambio.java
@@ -2,13 +2,30 @@ package usuarios;
 
 import intercambios.Intercambio;
 import productos.ProductoSegundaMano;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EmpleadoIntercambio extends Empleado {
+
+    private List<ProductoSegundaMano> productosPorValorar;
+    private List<Intercambio> intercambiosPendientes;
     
     public EmpleadoIntercambio(String nombre, String contraseña) {
         super(nombre, contraseña);
+        this.productosPorValorar = new ArrayList<>();
+        this.intercambiosPendientes = new ArrayList<>(); 
     }
+
+    //SETTERS//
+    public void addProductoParaValorar(ProductoSegundaMano p) {this.productosPorValorar.add(p);}
+    public void addIntercambio(Intercambio i) {this.intercambiosPendientes.add(i);}
+
+    //GETTERS//
+    public List<ProductoSegundaMano> getProductosPorValorar() {return this.productosPorValorar;}
+    public List<Intercambio> getIntercambiosPendientes() {return this.intercambiosPendientes;}
+
 
     public void confirmarIntercambio(Intercambio i) { 
         i.setIntercambiado(true);
@@ -17,21 +34,17 @@ public class EmpleadoIntercambio extends Empleado {
     public void valorarProducto(ProductoSegundaMano p) {
         
         Scanner teclado = new Scanner(System.in);
-        String buffer;
         int valoracionEmpleado = 0;
 
         while(valoracionEmpleado == 0) {
             try {
-                buffer = teclado.nextLine();
-                valoracionEmpleado = Integer.parseInt(buffer);
+                valoracionEmpleado = teclado.nextInt();
                 p.setValoracionEmpleado(valoracionEmpleado);
-            } catch (NumberFormatException e) {
+            } catch (InputMismatchException e) {
                 System.err.println("Solo se admiten números");
+            }
         }
-        }
-        
-
- 
+        teclado.close();
     }
 
     public void marcarIntercambioListo(Intercambio i) {}
