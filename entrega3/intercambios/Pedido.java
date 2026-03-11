f86bc0092da80c5ebb9a662157fb9995e285f06d
ale <alejandra.zurdo@estudiante.uam.es>
2026-03-11 18:26:30 +0100

    pedido

--- entrega3/intercambios/Pedido.java
+++ entrega3/intercambios/Pedido.java
@@ -4,7 +4,6 @@ public class Pedido {
     private EstadoPedido estadoPedido;
 
     public Pedido() {
-        this.estadoPedido = EstadoPedido.EN_CARRITO;
     }
 
     public EstadoPedido getEstadoPedido() {
@@ -13,9 +12,5 @@ public class Pedido {
 
     public void setEstadoPedido(EstadoPedido estadoPedido) {
         this.estadoPedido = estadoPedido;
-    }
-
-    public void setDatos(String datos) {
-        /** faltan cosas cuando hagamos pedido bien hecho*/
     }
 }
