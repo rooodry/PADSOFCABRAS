d8ef700110b2256fac5520aeb9cdd854b6ab9ed5
ale <alejandra.zurdo@estudiante.uam.es>
2026-03-11 17:53:52 +0100

    empleado y ususario

--- entrega3/usuarios/Usuario.java
+++ entrega3/usuarios/Usuario.java
@@ -2,18 +2,18 @@ package usuarios;
 
 public class Usuario {
     private final String nombreUsuario;
-    private String contrasena;
+    private String contraseña;
 
-    public Usuario(String nombreUsuario, String contrasena) {
+    public Usuario(String nombreUsuario, String contraseña) {
         this.nombreUsuario = nombreUsuario;
-        this.contrasena = contrasena;
+        this.contraseña = contraseña;
     }
 
     public String getNombreUsuario() {
         return nombreUsuario;
     }
 
-    public String getContrasena() {
-        return contrasena;
+    public String getContraseña() {
+        return contraseña;
     }
 }
