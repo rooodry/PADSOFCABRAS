273bfc48b6bc023cb963e162780c989651e94db6
ale <alejandra.zurdo@estudiante.uam.es>
2026-03-11 17:44:32 +0100

    usuario

---
+++ entrega3/usuarios/Usuario.java
@@ -0,0 +1,19 @@
+package usuarios;
+
+public class Usuario {
+    private final String nombreUsuario;
+    private String contrasena;
+
+    public Usuario(String nombreUsuario, String contrasena) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+}
