8867bc95fe70ce204745475486ace7614e9ecbd5
bea <bgallorente@gmail.com>
2026-03-11 17:30:25 +0100

    cartera

---
+++ entrega3/usuarios/Cartera.java
@@ -0,0 +1,28 @@
+package usuarios;
+
+import java.util.ArrayList;
+import java.util.List;
+import productos.ProductoSegundaMano;
+
+public class Cartera {
+    private int numProductos;
+    private List<ProductoSegundaMano> productos;
+
+    public cartera() {
+        this.productos = new ArrayList<>();
+        this.numProductos = 0;
+    }
+
+    public void añadirProducto(ProductoSegundaMano productoSegundaMano) {
+        this.productos.add(productoSegundaMano);
+        this.numProductos = this.productos.size();
+    }
+
+    public List<ProductoSegundaMano> getProductosCartera() {
+        return this.productos;
+    }
+
+    public int getNumProductos() {
+        return numProductos;
+    }
+}
