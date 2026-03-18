a3b2cede914f72b9aecd664dbf2da147d55cb4b5
ale <alejandra.zurdo@estudiante.uam.es>
2026-03-18 18:43:02 +0100

    cambios

--- entrega3/src/usuarios/EmpleadoPedido.java
+++ entrega3/src/usuarios/EmpleadoPedido.java
@@ -1,23 +1,24 @@
 package usuarios;
-
+ 
+import java.util.Date;
+ 
 import compras.Pedido;
-import utilidades.EstadoPedido;
-
+import compras.EstadoPedido;
+ 
 public class EmpleadoPedido extends Empleado {
-
+ 
     public EmpleadoPedido(String nombreUsuario, String contraseña) {
         super(nombreUsuario, contraseña);
     }
-
-    public void editarPedido(Pedido p) {
-        /* necesito que hagamos la clase Pedido por completo antes de poder terminar la 
-         función esta. */
+ 
+    public void editarPedido(Pedido p, Date nuevaFechaRecogida) {
+        p.setFechaRecogida(nuevaFechaRecogida);
     }
-
+ 
     public void marcarComoListo(Pedido p) {
         p.setEstadoPedido(EstadoPedido.LISTO);
     }
-
+ 
     public void entregarPedido(Pedido p) {
         p.setEstadoPedido(EstadoPedido.ENTREGADO);
     }
